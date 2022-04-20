90646243726f9f5a30d02f08661b8e123758c569
abrar shams chowdhury <abrar.chowdhury99@gmail.com>
2022-04-20 16:22:21 +0600

    initial settup commit

---
+++ index.js
@@ -0,0 +1,62 @@
+// "\x1b[35m" magenta
+console.log("\n \n \n \n \n \n")
+
+console.log( false || "or False" )
+console.log( true || "or True" )
+
+console.log( false && "%cor False" )
+console.log( true && "or True" )
+
+
+//  Block-Scoped Declarations
+
+
+//  Spread /  Rest
+
+
+// Default Parameter Values
+
+
+// Destructuring
+
+
+// Object literal Extensions
+
+
+// Template Literals
+
+
+// Arrow Functions
+
+// For..of Loops
+
+// Regular Expresssions
+
+// Number Literal Extenstions
+
+// Unicode
+
+// Symbols
+
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("\n \n \n \n \n \n")
