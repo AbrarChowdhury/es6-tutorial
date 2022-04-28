71f46929c75cca62e680825a6711d82b5a1b2967
abrar shams chowdhury <abrar.chowdhury99@gmail.com>
2022-04-28 10:42:29 +0600

    include arrow funcs and for-of

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { kill } = require("nodemon/lib/monitor/run")
+
 // "\x1b[35m" magenta
 console.log("\n \n \n \n \n \n")
 
@@ -15,9 +17,9 @@ console.log("\n \n \n \n \n \n")
     // (unsurprisingly) block scoping. This means all we need is a pair of { .. } to create a scope
 
 
-    // let a = 2
+    // var a = 2
     // {
-    //     let a = 3
+    //     var a = 3
     //     {
     //         console.log(a)
     //     }
@@ -31,9 +33,11 @@ console.log("\n \n \n \n \n \n")
     // var a = 2
 
     // console.log(b)
-    // let b = 2
+    // var b = 2
+    
+    
 
-    // The const declarations
+    // const declarations
     //   const a = 2
     //   a = 4
     // const person = {
@@ -46,14 +50,14 @@ console.log("\n \n \n \n \n \n")
 //  Spread /  Rest
     // const arr = [ 1,2,4]
     // const arr2 = [ "a", "b", "c"]
-    // console.log(...arr)
-    // console.log(["a",...arr])
+    // console.log(...arr2)
+    // console.log([...arr,"a"])
 
     // //gather
-    // function foo (x, y, ...z){
-    //     console.log(x,y,z)
+    // function foo (x, y, a, ...z){
+    //     console.log(z)
     // }
-    // foo(1,2,3,4,5)
+    // foo(1,2,3,4,5,"a")
 
 
 // Default Parameter Values
@@ -62,22 +66,26 @@ console.log("\n \n \n \n \n \n")
     //     y = y || 5
     //     console.log(x+y)
     // }
+    // foo(0,3)
 
     //  what if you want to pass in 0
     // function foo(x,y){
     //     x = (x !== undefined) ? x:1
     //     y = (y !== undefined) ? y:5
+
     //     console.log(x+y)
     // }
-    // foo( undefined, 1 )
+
+    // foo( undefined,4)
     
     
     //what if you need to pass in undefined
-    // function foo(x,y){
+    // function foo(x,y,z){
     //     x = (0 in arguments) ? x:1
     //     y = (1 in arguments) ? y:5
     //     console.log(x+y)
     // }
+    // foo(undefined, 5)
 
     //turns out you cannot omit arguments in the middle or at the beginning of the arguments list
     
@@ -101,18 +109,20 @@ console.log("\n \n \n \n \n \n")
     //     return {name:"Harry", house:"Gryffindor"} 
     // }
 
+
     // let tmp = getArr(), a=tmp[0], b=tmp[1], c=tmp[2]
     // console.log( a, b, c)
 
-    // let tmp = getObj(), name = tmp.name, house=tmp.house
-    // console.log(name+" belongs to the "+ house + " house")
+    // let tmp = getObj(), namea = tmp.name, house=tmp.house
+    // console.log(namea+" belongs to the "+ house + " house")
 
     //The ES6 way
     // let [a, b, c] = getArr()
     // console.log( a, b, c)
 
-    // let { name, house } = getObj() 
-    // console.log(name+" belongs to the "+ house + " house")
+    // let { name, ...rest } = getObj() 
+    // console.log(name+" belongs to the "+ rest.house + " house")
+  
     
     //what if you want use name the variables differently
 
@@ -142,10 +152,13 @@ console.log("\n \n \n \n \n \n")
     // foo([1])
     // foo([])
 
-    // function foo ({x,y}){
+    // function foo (obj){
+    //     let {x,y}=obj
     //     console.log(x,y)
     // }
-    // foo( {y: 2, x:4} )
+
+    // const obj = {y: 2, x:4}
+    // foo( obj )
     // foo( { y: 54} )
     // foo( {} )
 
@@ -155,15 +168,16 @@ console.log("\n \n \n \n \n \n")
 // Object literal Extensions
     // let x=1, y=5,
     // obj = {
-    //     x:x,
-    //     y:y
+    //     x,
+    //     y,
+    //     name:"mehedi"
     // }
     // let x=1, y=5,
     // obj = { x, y }
 
-    // console.log(obj.x, obj.y)
+    // console.log(obj.x, obj.y, obj)
     // let obj ={
-    //     x: function(){
+    //     x: function (){
     //         //does something
     //     }
     // }
@@ -179,22 +193,87 @@ console.log("\n \n \n \n \n \n")
     // let obj = {
     //     x(){
     //         console.log("hello")
+    //         this.x()
     //     },
     // }
+
+    // obj.x()
     
 
 // Template Literals
     // let name = "Ron"
     // let greeting = "Hello " + name + "!"
     // let greeting = `Hello ${name}`
-    // let greeting = `Hello ${name.toUpperCase()}`
+    // let greeting = `Hello ${name.toUpperCase()}hibye`
+    
     // console.log(greeting)
 
 
 // Arrow Functions
-    //not just shorter syntaxt
+
+    // const f1 = () => 12;
+    // const f2 = x => x * 2
+    // const f3 = (x,y) => {
+    //     let z = x * 2 + y
+    //     y++
+    //     x*=3
+    //     return (x + y + z)/2
+    // }
+    // console.log(f1())
+
+    // console.log(f2(5))
+    
+    // console.log(f3(1,10))
+
+    // let a = [1,2,3,4,5]
+    //mapping without a=>funcs
+    // a =  a.map( e => e * 2)
+    // console.log(a)
+    // do we need to use it everywhere now?
+    // readability is inversely proportional to the length of the function
+
+    //not just a shorter syntaxt
+    
+    //this 
+        // const obj ={
+        //     f1(){
+        //         console.log(this)
+        //     },
+        // }
+
+        // function f2(){
+        //     console.log(this)
+        // }
+
+    // const controller = {
+    //     makeRequest: function (){
+    //         let self = this
+    //         btn.addEventListerner("click", function(){
+    //             self.makeRequest()
+    //         }, false)
+    //     }
+    // }
+    // const controller = {
+    //     makeRequest: function (){
+    //         btn.addEventListerner("click", ()=>{
+    //             this.makeRequest()
+    //         }, false)
+    //     }
+    // }
+    // .bind(this)
+
 
 // For..of Loops
+
+    // let a = ["a","b","c","d","e"]
+    // a = "Hello world"
+    // for( let i in a){
+    //     console.log(i)
+    // }
+    // for( let val of a){
+    //     console.log(val)
+    // }
+
 
 // Regular Expresssions
 
